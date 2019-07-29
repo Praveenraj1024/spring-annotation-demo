34c2e49c3273dda33a9e4e087087180e5c71cc46
praveen <praveenrajjoth@gmail.com>
2019-07-29 23:59:55 +0530

    Annotation Task 3 completed.

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -2,11 +2,8 @@ package com.stackroute;
 
 import com.stackroute.domain.ConfigurationClass;
 import com.stackroute.domain.Movie;
-//import org.springframework.beans.factory.BeanFactory;
-//import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-//import org.springframework.core.io.ClassPathResource;
 
 
 public class App 
@@ -15,10 +12,6 @@ public class App
     {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigurationClass.class);
         Movie movie1 = applicationContext.getBean("movie1", Movie.class);
-        Movie movie2 = applicationContext.getBean("movie2", Movie.class);
-        Movie vedhalam = applicationContext.getBean("vedhalam", Movie.class);
         System.out.println(movie1);
-        System.out.println(vedhalam);
-        System.out.println(movie2);
     }
 }
--- src/main/java/com/stackroute/domain/ConfigurationClass.java
+++ src/main/java/com/stackroute/domain/ConfigurationClass.java
@@ -12,6 +12,7 @@ import org.springframework.core.env.Environment;
 public class ConfigurationClass {
     @Autowired
     private Environment environment;
+
     @Bean(name = {"actor1"})
     public Actor getActor() {
         Actor actor1 = new Actor(environment.getProperty("actor1.name"),
@@ -20,25 +21,12 @@ public class ConfigurationClass {
         return actor1;
     }
 
-    @Bean(name = {"actor2"})
-    public Actor getAjith() {
-        Actor actor1 = new Actor(environment.getProperty("actor2.name"),
-                environment.getProperty("actor2.gender"),
-                Integer.parseInt(environment.getProperty("actor2.age")));
-        return actor1;
-    }
-
 
     @Scope("prototype")
-    @Bean(name = {"movie1", "movie2"})
+    @Bean(name = {"movie1"})
     public Movie getMovie() {
-        Movie movie1 = new Movie(getActor());
+        Movie movie1 = new Movie();
         return movie1;
     }
 
-    @Bean(name = {"vedhalam"})
-    public Movie getVedhalam() {
-        Movie vedhalam = new Movie(getAjith());
-        return vedhalam;
-    }
 }
